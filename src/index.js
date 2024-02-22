4ec3f39ddf1b8561ccc692e6a498b8b2750f7f65
Damon Haase <damon.haase@gmail.com>
2024-02-22 17:47:58 -0500

    Remembered how classes work. Add required code snippet.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './style.css';
 import './meyerReset.css';
+
+//TOP self limitation function to prevent accessing an index out or range.
+//Must be included whenever accessing a bucket with an index.
+//function is WIP. Probably just include the snippet where it's needed first.
+function testIndex(index) {
+  if (index < 0 || index >= buckets.length) {
+    throw new Error("Trying to access index out of bound");
+  }
+}
+
+
+
+
+class HashMap {
+    hash(key) {
+        let hashCode = 0;
+            
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+      
+        return hashCode;
+      }
+
+}
+let test = new HashMap();
+let testCode = test.hash('Lim');
+console.log(testCode);
