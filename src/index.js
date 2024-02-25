4bb044bca6d5720a87a52e333fccfcab9c19b85e
Damon Haase <damon.haase@gmail.com>
2024-02-25 10:50:14 -0500

    Before refactor to factory function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import './meyerReset.css';
+import LinkedList from './linkedList';
 
 //TOP self limitation function to prevent accessing an index out or range.
 //Must be included whenever accessing a bucket with an index.
@@ -14,19 +15,35 @@ function testIndex(index) {
 
 
 class HashMap {
-    hash(key) {
-        let hashCode = 0;
-            
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i);
-          hashCode = hashCode % 16;
-        }
-      
-        return hashCode;
+  capacity = 16;
+  loadFactor = .75;
+  buckets = Array.from({length: this.capacity});
+
+  hash(key) {
+      let hashCode = 0;
+          
+      const primeNumber = 31;
+      for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        hashCode = hashCode % this.capacity;
       }
+    
+      return hashCode;
+    }
+
+  set(key, value) {
+    const index = hash(key);
+    const bucket = this.buckets[index];
+    if (bucket === null) 
+      bucket = [key, value];
+    else if (bucket.key === key)
+      bucket.value = value;
+   // else need to make a linked listz
+  }
 
 }
+
 let test = new HashMap();
-let testCode = test.hash('');
-console.log(testCode);
+
+let testList = new LinkedList();
+
