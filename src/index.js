a55ddddbc420962deb6d2346fe13f8cb44e70fc1
Damon Haase <damon.haase@gmail.com>
2024-02-26 15:17:24 -0500

    Refactor to use factory functions instead of class

--- src/index.js
+++ src/index.js
@@ -2,48 +2,40 @@ import './style.css';
 import './meyerReset.css';
 import LinkedList from './linkedList';
 
-//TOP self limitation function to prevent accessing an index out or range.
-//Must be included whenever accessing a bucket with an index.
-//function is WIP. Probably just include the snippet where it's needed first.
-function testIndex(index) {
-  if (index < 0 || index >= buckets.length) {
-    throw new Error("Trying to access index out of bound");
-  }
-}
+
+
+const createHashTable = () => {
+  let capacity = 16;
+  let loadFactor = .75;
+  let buckets = [];
 
 
 
-
-class HashMap {
-  capacity = 16;
-  loadFactor = .75;
-  buckets = Array.from({length: this.capacity});
-
-  hash(key) {
-      let hashCode = 0;
-          
-      const primeNumber = 31;
-      for (let i = 0; i < key.length; i++) {
-        hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        hashCode = hashCode % this.capacity;
-      }
-    
-      return hashCode;
+  function hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % capacity;
     }
-
-  set(key, value) {
+ 
+    return hashCode;
+  }
+  
+  function set(key, value) {
     const index = hash(key);
-    const bucket = this.buckets[index];
-    if (bucket === null) 
-      bucket = [key, value];
-    else if (bucket.key === key)
-      bucket.value = value;
-   // else need to make a linked listz
+    testIndex(index);
+    const bucket = buckets[index];
+    if (!bucket) bucket = LinkedList;
   }
 
+  //TOP self limitation function to prevent accessing an index out of range.
+  //Must be included whenever accessing a bucket with an index.
+  function testIndex(index) {
+    if (index < 0 || index >= buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+  }
+  return {hash, set}
 }
-
-let test = new HashMap();
-
-let testList = new LinkedList();
-
