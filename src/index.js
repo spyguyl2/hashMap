857584140444a2b8cffba3c833ec01205d2547f3
Damon Haase <damon.haase@gmail.com>
2024-02-27 20:34:27 -0500

    Finish set(). Setup rest of functions.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import './style.css';
 import './meyerReset.css';
 import newList from './linkedList';
 
-
-
 const createHashTable = () => {
+  let fullBuckets = 0;
   let capacity = 16;
   let loadFactor = .75;
   let buckets = initializeBuckets();
@@ -12,7 +11,64 @@ const createHashTable = () => {
   function set(key, value) {
     const index = hash(key);
     testIndex(index);
-    buckets[index].append(value)
+    const bucket = buckets[index];
+    if (!bucket.getHead()) {
+      bucket.prepend(key, value);
+      fullBuckets++;
+    }
+    else {
+      let currentNode = bucket.getHead();
+      while (currentNode.nextNode || currentNode) {
+        if (currentNode.key === key) return currentNode.value = value;
+        currentNode = currentNode.nextNode;
+      }
+      bucket.prepend(key, value);
+    }
+  }
+
+  function get(key) {
+    const index = hash(key);
+    testIndex(index);
+    const bucket = buckets[index];
+    if (bucket.contains(key)) {
+      let currentNode = bucket.getHead();
+      while (currentNode || currentNode.nextNode) {
+        if (currentNode.key === key) return currentNode.value;
+        currentNode = currentNode.nextNode;
+      }
+      return null;
+    }
+    else return null;
+  }
+
+  function has(key) {
+
+  }
+
+  function remove(key) {
+
+  }
+
+  function length() {
+
+  }
+
+  function clear() {
+    buckets.forEach((bucket) => {
+      bucket = null;
+    });
+  }
+
+  function keys() {
+
+  }
+
+  function values() {
+
+  }
+
+  function entries() {
+
   }
 
   function hash(key) {
@@ -35,21 +91,37 @@ const createHashTable = () => {
   function initializeBuckets() {
     let buckets = [];
     for (let i  = 0; i < capacity; i++) {
-      buckets.push(newList);
+      buckets.push(newList());
     }
     return buckets;
   }
 
   //TOP self limitation function to prevent accessing an index out of range.
-  //Must be included whenever accessing a bucket with an index.
+  //Must be included whenever accessing a bucket with an index (hash code).
   function testIndex(index) {
     if (index < 0 || index >= buckets.length) {
       throw new Error("Trying to access index out of bound");
     }
   }
-  return {set, hash}
+  return {set, hash, buckets, get, has, remove, length, clear, keys, values, entries}
 }
 
 
 let testTable = createHashTable();
 testTable.set('john', 'smith');
+testTable.set('Migh', 'kawk');
+testTable.set('johbobn', 'shmittyweberjemenmenjensen');
+testTable.set('bob', 'i shouldnt see this');
+testTable.set('bob', 'or this');
+testTable.set('bob', 'oooooor this');
+testTable.set('bob', 'not this one either');
+testTable.set('bob', 'Only this one.');
+
+testTable.clear();
+
+for (let i = 0; i < testTable.buckets.length; i++) {
+  const element = testTable.buckets[i];
+  if(element.getSize() > 0) console.log(element.toString());
+}
+
+console.log(testTable.get('bob'));
